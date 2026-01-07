aae33812c01f7d0f2d35c6b681843827c0b2535e
Sam A.S. Welch <116264477+sawelch-NIVA@users.noreply.github.com>
2026-01-07 12:53:39 +0100

    Removed old temp_R and images dirs

--- temp_R/scratchpad_loading.R
+++
@@ -1,9 +0,0 @@
-if (!exists("requirements_loaded")) {
-  source("temp_R/scratchpad.r")
-  load_all_requirements()
-}
-
-sites <- fread_all_module_files(
-  get_literature_csv_paths(module = "Sites"),
-  initialise_sites_tibble
-)
